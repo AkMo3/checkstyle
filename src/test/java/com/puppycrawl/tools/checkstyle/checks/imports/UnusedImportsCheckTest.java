1523ae5297b9392af09b5e05b84e9f2a412078d9
Akash Mondal <69365878+AkMo3@users.noreply.github.com>
2021-01-22 01:54:14 +0530

    Revert "Issue #4219: UnusedImports doesn't recognize imports shadowed by inner types"
    This reverts commit a07b1a9ef77d2a030dce12e952e9064aef2afaca.

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/UnusedImportsCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/UnusedImportsCheckTest.java
@@ -182,8 +182,6 @@ public class UnusedImportsCheckTest extends AbstractModuleTestSupport {
             TokenTypes.VARIABLE_DEF,
             TokenTypes.RECORD_DEF,
             TokenTypes.COMPACT_CTOR_DEF,
-            TokenTypes.OBJBLOCK,
-            TokenTypes.SLIST,
         };
 
         assertArrayEquals(expected, actual, "Default required tokens are invalid");
@@ -211,8 +209,6 @@ public class UnusedImportsCheckTest extends AbstractModuleTestSupport {
             TokenTypes.VARIABLE_DEF,
             TokenTypes.RECORD_DEF,
             TokenTypes.COMPACT_CTOR_DEF,
-            TokenTypes.OBJBLOCK,
-            TokenTypes.SLIST,
         };
 
         assertArrayEquals(expected, actual, "Default acceptable tokens are invalid");
@@ -277,18 +273,6 @@ public class UnusedImportsCheckTest extends AbstractModuleTestSupport {
         verify(checkConfig,
                 getNonCompilablePath("InputUnusedImportsRecordsAndCompactCtors.java"),
                 expected);
-    }
-
-    @Test
-    public void testShadowedImports() throws Exception {
-        final DefaultConfiguration checkConfig = createModuleConfig(UnusedImportsCheck.class);
-        final String[] expected = {
-            "5:8: " + getCheckMessage(MSG_KEY, "java.util.Map"),
-            "6:8: " + getCheckMessage(MSG_KEY, "java.util.Set"),
-            "9:8: " + getCheckMessage(MSG_KEY, "com.puppycrawl.tools.checkstyle.checks.imports."
-                    + "unusedimports.InputUnusedImportsShadowed"),
-        };
-        verify(checkConfig, getPath("InputUnusedImportsShadowed.java"), expected);
     }
 
 }
